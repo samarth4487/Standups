1cf6a5511f09995e561fef123e088421811d2f85
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2023-11-27 15:36:23 +0530

    Initial Commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Samarth Paboowal on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
